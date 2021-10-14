025be74cdd664731c33669e8cb85d1b3eb3f1fda
oivzhich <oivzhich@gmail.com>
2021-10-14 11:19:11 +0300

    my-app-config added (Okta integration)

---
+++ src/app/config/my-app-config.ts
@@ -0,0 +1,8 @@
+export default {
+  oidc: {
+    clientId: '0oa27s5tv3BZM8Cmn5d7',
+    issuer: 'https://dev-45738659.okta.com/oauth2/default',
+    redirectUri: 'http://localhost:4200/login/callback',
+    scopes: ['openid', 'profile', 'email']
+  }
+}
